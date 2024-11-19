ecf9709913fc2324e694ece91faae1f8eb718492
sibisaan <goestiancompany@gmail.com>
2024-11-20 03:29:33 +0700

    Add files via upload

---
+++ login10.py
@@ -0,0 +1,7 @@
+<iframe
+  width="100%"
+  height="400"
+  frameborder="0" style="border:0"
+  src="https://www.google.com/maps/embed?pb=!ujDwJ5YLG8cJyjmn6!1d15865.197981623694!2d106.8049218!3d-6.227750449999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f09e49f2b56d%3A0x3ff0e1d5680b10a6!2sMonumen%20Nasional!5e0!3m2!1sen!2sid!4v1625102098287!5m2!1sen!2sid"
+  allowfullscreen>
+</iframe>
